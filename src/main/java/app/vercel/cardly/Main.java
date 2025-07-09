5675e4a6cb5b4e9c17f8bd1e2964a33ca723e3bb
서민혁 <mahns1201@gmail.com>
2025-07-09 18:03:28 +0900

    Init spring boot

---
+++ src/main/java/app/vercel/cardly/Main.java
@@ -0,0 +1,11 @@
+package app.vercel.cardly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
