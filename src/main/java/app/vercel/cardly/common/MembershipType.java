128a00bf300a842d193d259fac1ab867ddde55af
서민혁 <mahns1201@gmail.com>
2025-07-11 15:43:42 +0900

    Add spotless as google java format

---
+++ src/main/java/app/vercel/cardly/common/MembershipType.java
@@ -0,0 +1,1 @@
+package app.vercel.cardly.common;import com.fasterxml.jackson.annotation.JsonValue;public enum MembershipType {    INTERN("INTERN"),    STAFF("STAFF"),    MANAGER("MANAGER");    private final String value;    MembershipType(String value) { this.value = value; }    @JsonValue    public String getValue() { return value; }}
---
+++ src/main/java/app/vercel/cardly/domain/membership/MembershipRepository.java
@@ -0,0 +1,1 @@
+package app.vercel.cardly.domain.membership;import app.vercel.cardly.common.MembershipType;import org.springframework.data.jpa.repository.JpaRepository;import java.util.Optional;public interface MembershipRepository extends JpaRepository<Membership, Long> {    Optional<Membership> findByName(MembershipType name);}
