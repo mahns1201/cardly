72cb9be9c976b5908ac2894f2e4f62f9e368cf8a
서민혁 <mahns1201@gmail.com>
2025-07-11 14:54:30 +0900

    Feat user role setting

---
+++ src/main/java/app/vercel/cardly/common/RoleType.java
@@ -0,0 +1,1 @@
+package app.vercel.cardly.common;import com.fasterxml.jackson.annotation.JsonValue;public enum RoleType {    ADMIN("ADMIN"),    USER("USER");    private final String value;    RoleType(String value) { this.value = value; }    @JsonValue    public String getValue() { return value; }}
---
+++ src/main/java/app/vercel/cardly/domain/role/RoleRepository.java
@@ -0,0 +1,1 @@
+package app.vercel.cardly.domain.role;import app.vercel.cardly.common.RoleType;import org.springframework.data.jpa.repository.JpaRepository;import java.util.Optional;public interface RoleRepository extends JpaRepository<Role, Long> {    Optional<Role> findByName(RoleType name);}
